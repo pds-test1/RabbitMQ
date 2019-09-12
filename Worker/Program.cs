1e04b4ab17c77a5823f6fea9014cd13e18ce93a9
gandalf <814511895@qq.com>
2019-09-12 13:49:40 +0800

    代码对齐

--- Worker/Program.cs
+++ Worker/Program.cs
@@ -4,44 +4,50 @@ using System.Threading;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
-namespace Worker {
-    class Program {
-    public static void Main () {
-    //1.创建基于本地的连接工厂
-            var factory = new ConnectionFactory () { HostName = "localhost" };
+namespace Worker
+{
+    class Program
+    {
+        public static void Main()
+        {
+            //1.创建基于本地的连接工厂
+            var factory = new ConnectionFactory() { HostName = "localhost" };
             //2. 建立连接
-            using (var connection = factory.CreateConnection ()) {
+            using (var connection = factory.CreateConnection())
+            {
                 //3. 创建频道
-                using (var channel = connection.CreateModel ()) {
+                using (var channel = connection.CreateModel())
+                {
                     //4. 申明队列(指定durable:true,告知rabbitmq对消息进行持久化)
-                    channel.QueueDeclare (queue: "work_queue", durable : true, exclusive : false, autoDelete : false, arguments : null);
+                    channel.QueueDeclare(queue: "work_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
 
                     //设置prefetchCount : 1来告知RabbitMQ，在未收到消费端的消息确认时，不再分发消息，也就确保了当消费端处于忙碌状态时，不再分配任务。
-                    channel.BasicQos (prefetchSize : 0, prefetchCount : 1, global : false);
+                    channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
 
                     //5. 构造消费者实例
-                    var consumer = new EventingBasicConsumer (channel);
+                    var consumer = new EventingBasicConsumer(channel);
                     //6. 绑定消息接收后的事件委托
-                    consumer.Received += (model, ea) => {
+                    consumer.Received += (model, ea) =>
+                    {
                         var body = ea.Body;
-                        var message = Encoding.UTF8.GetString (body);
-                        Console.WriteLine (" [x] Received {0}", message);
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
 
                         //7. 模拟耗时
-                        int dots = message.Split ('.').Length - 1;
-                        Thread.Sleep (dots * 1000);
-                        Console.WriteLine (" [x] Done");
+                        int dots = message.Split('.').Length - 1;
+                        Thread.Sleep(dots * 1000);
+                        Console.WriteLine(" [x] Done");
 
                         // 8. 发送消息确认信号（手动消息确认）
-                        channel.BasicAck (deliveryTag : ea.DeliveryTag, multiple : false);
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
                     };
                     //9. 启动消费者
                     //autoAck:true；自动进行消息确认，当消费端接收到消息后，就自动发送ack信号，不管消息是否正确处理完毕
                     //autoAck:false；关闭自动消息确认，通过调用BasicAck方法手动进行消息确认
-                    channel.BasicConsume (queue: "work_queue", autoAck : false, consumer : consumer);
+                    channel.BasicConsume(queue: "work_queue", autoAck: false, consumer: consumer);
 
-                    Console.WriteLine (" Press [enter] to exit.");
-                    Console.ReadLine ();
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
                 }
             }
         }
